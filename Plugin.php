07a6408f8b3e94b572768c524944a67ef3e3a28f
nan2xiao <3134955652@qq.com>
2024-05-05 15:27:28 +0800

    Initial commit

---
+++ Plugin.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * 限制評論字數和內容
+ * @author molikai
+ * @package CommentRestrict
+ * @version 1.1.0
+ * @link https://github.com/molikai-work/commentRestrict
+ */
+
+class CommentRestrict_Plugin implements Typecho_Plugin_Interface {
+    public static function activate()
+    {
+        Typecho_Plugin::factory('Widget_Feedback')->comment = array('CommentRestrict_Plugin', 'checkComment');
+        return _t('插件已成功激活');
+    }
+
+    public static function deactivate()
+    {
+        return _t('插件已停用');
+    }
+
+    public static function config(Typecho_Widget_Helper_Form $form)
+    {
+        $minLength = new Typecho_Widget_Helper_Form_Element_Text('minLength', NULL, '5', _t('評論最少字數'));
+        $form->addInput($minLength);
+
+        $maxLength = new Typecho_Widget_Helper_Form_Element_Text('maxLength', NULL, '200', _t('評論最多字數'));
+        $form->addInput($maxLength);
+
+        $forbiddenWords = new Typecho_Widget_Helper_Form_Element_Text('forbiddenWords', NULL, 'ces1,敏感詞2', _t('違禁詞列表（使用逗號分隔）'));
+        $form->addInput($forbiddenWords);
+    }
+
+    public static function personalConfig(Typecho_Widget_Helper_Form $form) {
+        // ...
+    }
+
+    public static function checkComment($comment, $post) {
+            $options = Typecho_Widget::widget('Widget_Options')->plugin('CommentRestrict');
+
+            $minLength = intval($options->minLength);
+            $maxLength = intval($options->maxLength);
+
+            $forbiddenWords = array_map('trim', explode(',', $options->forbiddenWords));
+
+            $content = $comment['text'];
+            $contentLength = mb_strlen($content, 'utf-8');
+
+            if ($contentLength < $minLength || $contentLength > $maxLength) {
+               throw new Typecho_Widget_Exception(_t('評論字數不符合要求，請輸入 %d-%d 字之間的評論.', $minLength, $maxLength));
+            }
+
+            foreach ($forbiddenWords as $word) {
+                if (!empty($word) && mb_stripos($content, $word, 0, 'utf-8') !== false) {
+                    throw new Typecho_Widget_Exception(_t('評論中包含違禁詞，請檢查並修改。'));
+            }
+        }
+
+        return $comment;
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# commentRestrict
+
+注意：本插件可能不與額外的主題或插件兼容，作者不為額外的主題或插件的兼容性做擔保。
+
+請下載存儲庫中的 `Plugin.php` 文件，然後在您的 Typecho 程式存儲空間的 `/usr/plugins/` 目錄下新建一個文件夾 `CommentRestrict`，然後將下載的 `Plugin.php` 文件放入即可，
+下一步您需要進入您的 Typecho 程式控制台啟用這個插件 `CommentRestrict` 並進入設置頁面簡單設置插件。
+
+插件僅包含功能：
+
+- 評論字數顯示（最多、最少）
+- 關鍵詞限制
